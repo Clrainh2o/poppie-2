e1885b4dee3d5d8b66aa3b741b965f72909407fa
Clrainh2o <caleb@rainwater.graphics>
2025-01-26 15:29:12 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).on("scroll", function () {
+	if ($(document).scrollTop() > 100) {
+		$("header").addClass("small");
+	} else {
+		$("header").removeClass("small");
+	}
+});
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 6000); // Change image every 6 seconds (6000ms)
+}
